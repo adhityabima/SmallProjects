52b97abe5a1dd6703416c3dccfc7f38cac10ca23
adhtya_b <laxusfarhami@gmail.com>
2021-02-10 20:40:45 +0700

    Palindrome Project

---
+++ palindrome-project/app.js
@@ -0,0 +1,10 @@
+const arrAlphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k",
+ "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+
+function palindrome (str){
+    if(str == "test"){
+        return true
+    }
+}
+
+console.log(arrAlphabet.includes('a'))
