987a2b71ae3383213ab64d4775dbeb418d7e5fad
adhtya_b <laxusfarhami@gmail.com>
2021-02-11 15:19:44 +0700

    Finished simple palindrome project

--- palindrome-project/app.js
+++ palindrome-project/app.js
@@ -1,9 +1,26 @@
 const otherChara = /[\W_]/g;
+const result = document.querySelector('#palinResult');
+const submitBtn = document.querySelector('#submit');
+const input = document.querySelector('#word');
+const form = document.querySelector('#palinForm')
 function palindrome (str){
     const wordReverse = str.replace(otherChara, '').toLowerCase().split('').reverse().join('');
     const wordOriginal = str.replace(otherChara, '').toLowerCase()
     if(wordReverse == wordOriginal){
-        return true;
+        result.innerHTML = `"${input.value}" is a Palindrome!`
     }
-    return false;
+    result.innerHTML = `"${input.value}" is not a Palindrome`
 }
+
+palinForm.addEventListener('submit',(e) => {
+    e.preventDefault()
+    palindrome(input.value)
+    input.value = "";
+})
+
+palinForm.addEventListener('reset',(e) =>{
+    e.preventDefault()
+    input.value = ""
+    result.innerHTML = ""
+})
+
